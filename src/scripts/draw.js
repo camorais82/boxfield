ba07f3f90d5ba9e472cda6ceecd52353ff11ed87
Jin Chen <jinchen93@gmail.com>
2017-05-29 23:54:15 -0700

    Add fill to the obstacles

--- src/scripts/draw.js
+++ src/scripts/draw.js
@@ -1,3 +1,5 @@
+const Utils = require("./utils");
+
 const Draw = {
   drawLeftSquare: (ctx, { x, y, size, offset }) => {
     ctx.beginPath();
@@ -15,6 +17,10 @@ const Draw = {
     ctx.moveTo(x - offset, y - offset);
     ctx.lineTo(x - offset, y - offset + size);
     ctx.lineTo(x, y + size);
+    ctx.lineTo(x, y);
+    ctx.lineTo(x - offset, y - offset);
+    ctx.fillStyle = "#000";
+    ctx.fill();
     ctx.stroke();
   },
 
@@ -33,7 +39,28 @@ const Draw = {
     ctx.moveTo(x + offset + size, y - offset);
     ctx.lineTo(x + offset + size, y + size - offset);
     ctx.lineTo(x + size, y + size);
+    ctx.lineTo(x + size, y);
+    ctx.lineTo(x + size + offset, y - offset);
+    ctx.fillStyle = "#000";
+    ctx.fill();
     ctx.stroke();
+  },
+
+  drawPlayer: ctx => {
+    const x = Utils.width / 2;
+    const y = Utils.height - 150;
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#84e0e9";
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 40, y + 100);
+    ctx.lineTo(x, y + 70);
+    ctx.lineTo(x - 40, y + 100);
+    ctx.lineTo(x, y);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fillStyle = "#84e0e9";
+    ctx.fill();
   },
 };
 
