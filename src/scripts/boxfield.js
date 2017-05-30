dd9ae425a404dcc84da74bea16a007f36cfc9375
Jin Chen <jinchen93@gmail.com>
2017-05-29 22:36:08 -0700

    Add entry file

--- src/scripts/boxfield.js
+++ src/scripts/boxfield.js
@@ -1,2 +1,14 @@
 require("../styles/main.scss");
-const canvas = document.querySelector("canvas");
+const Obstacle = require("./obstacle");
+const Utils = require("./utils");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.querySelector("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const ctx = canvas.getContext("2d");
+
+  const obstacle = new Obstacle(ctx);
+
+  obstacle.animate();
+});
