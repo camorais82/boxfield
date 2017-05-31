aec4508a35a5cd1f51b3612ad688c76bb30c1355
Jin Chen <jinchen93@gmail.com>
2017-05-30 22:16:10 -0700

    Add game view to to handle animation logic

--- src/scripts/boxfield.js
+++ src/scripts/boxfield.js
@@ -1,23 +1,22 @@
 require("../styles/main.scss");
-const Obstacle = require("./obstacle");
-const Draw = require('./draw');
 const Utils = require("./utils");
-const Field = require('./field');
+const GameView = require("./gameView");
 
 document.addEventListener("DOMContentLoaded", () => {
   const fieldCanvas = document.getElementById("field");
   fieldCanvas.width = Utils.width;
   fieldCanvas.height = Utils.height;
-
   const fieldCtx = fieldCanvas.getContext("2d");
-  const field = new Field(fieldCtx);
-  window.requestAnimationFrame(() => field.animate());
 
   const objCanvas = document.getElementById("objects");
   objCanvas.width = Utils.width;
   objCanvas.height = Utils.height;
-
   const objCtx = objCanvas.getContext("2d");
-  const obstacle = new Obstacle(objCtx);
-  window.requestAnimationFrame(() => obstacle.animate());
+
+  const gameView = new GameView({
+    fieldCtx,
+    objCtx,
+  });
+
+  gameView.start();
 });
